9864db0fb0a7effd9f978dc65ea4f195fb42c77c
Ross Chapman <ross.chapman@exygy.com>
2024-06-27 14:32:05 -0700

    Fixes types

--- app/models/Strapi.ts
+++ app/models/Strapi.ts
@@ -4,11 +4,11 @@ import { StrapiApi } from 'hooks/StrapiAPI';
   Model interfaces are a subtype of Strapi api responses that only include properties needed by consuming components
 */
 export namespace StrapiModel {
-  export interface Link extends StrapiApi.Link { }
+  export interface Link extends StrapiApi.LinkResponse { }
   export interface DynamicLink extends Omit<StrapiApi.DynamicLinkResponse, "__component"> { }
   export interface Footer extends StrapiApi.FooterResponse { }
   export interface Header extends StrapiApi.HeaderResponse { }
-  export interface Logo extends Pick<StrapiApi.Logo, "width" | "height" | "alternativeText" | "url"> { }
+  export interface Logo extends Pick<StrapiApi.LogoResponse, "width" | "height" | "alternativeText" | "url"> { }
   export interface NavigationMenu extends StrapiApi.NavigationMenuResponse {
   }
 }
