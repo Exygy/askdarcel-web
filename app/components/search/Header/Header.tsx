29ed21165b2a876b1f3f7fe093d7d032bc1f4cfd
Adrien Young <adrien.elizabeth@gmail.com>
2024-07-01 12:34:49 -0700

    big style edit for custom dropdown

--- app/components/search/Header/Header.tsx
+++ app/components/search/Header/Header.tsx
@@ -33,67 +33,69 @@ export const Header = ({
 
   return (
     <div className={styles.header}>
-      <div>
-        <h1 className="sr-only">{resultsTitle}</h1>
-        <Dropdown
-          categories={topLevelCategories}
-          currentCategory={currentCategory}
-          onCategoryChange={handleCategoryChange}
-          resultsTitle={resultsTitle}
-        />
-      </div>
-      <Button
-        onClick={() => {
-          setQrCodeModalOpen(true);
-        }}
-        addClass={`${styles.qrCodeBtn} ${
-          showHeaderQrCode ? styles.showBtn : ""
-        }`}
-        styleType="transparent"
-      >
-        <>
-          <img src={assetIcon("qr-code")} alt="QR code icon" />
-          <span className={styles.btnText}>Resource List QR Code</span>
-        </>
-      </Button>
-      <Button
-        iconName="fas fa-print"
-        iconVariant="before"
-        variant="secondary"
-        size="lg"
-        onClick={() => {
-          window.print();
-        }}
-        addClass={`${styles.printAllBtn} ${
-          showPrintResultsBtn ? styles.showBtn : ""
-        }`}
-      >
-        Print all results
-      </Button>
-      <QrCodeModal isOpen={qrCodeModalOpen} setIsOpen={setQrCodeModalOpen} />
-      <div className={styles.mapListToggleContainer}>
-        <button
-          type="button"
-          className={styles.mapListToggleBtn}
-          onClick={() => setExpandList(true)}
-        >
-          <span
-            className={`${styles.listIcon} ${
-              expandList ? styles.activeView : ""
-            }`}
+      <div className={styles.headerInner}>
+        <div>
+          <h1 className="sr-only">{resultsTitle}</h1>
+          <Dropdown
+            categories={topLevelCategories}
+            currentCategory={currentCategory}
+            onCategoryChange={handleCategoryChange}
+            resultsTitle={resultsTitle}
           />
-        </button>
-        <button
-          type="button"
-          className={styles.mapListToggleBtn}
-          onClick={() => setExpandList(false)}
+        </div>
+        <Button
+          onClick={() => {
+            setQrCodeModalOpen(true);
+          }}
+          addClass={`${styles.qrCodeBtn} ${
+            showHeaderQrCode ? styles.showBtn : ""
+          }`}
+          styleType="transparent"
         >
-          <span
-            className={`${styles.mapIcon} ${
-              !expandList ? styles.activeView : ""
-            }`}
-          />
-        </button>
+          <>
+            <img src={assetIcon("qr-code")} alt="QR code icon" />
+            <span className={styles.btnText}>Resource List QR Code</span>
+          </>
+        </Button>
+        <Button
+          iconName="fas fa-print"
+          iconVariant="before"
+          variant="secondary"
+          size="lg"
+          onClick={() => {
+            window.print();
+          }}
+          addClass={`${styles.printAllBtn} ${
+            showPrintResultsBtn ? styles.showBtn : ""
+          }`}
+        >
+          Print all results
+        </Button>
+        <QrCodeModal isOpen={qrCodeModalOpen} setIsOpen={setQrCodeModalOpen} />
+        <div className={styles.mapListToggleContainer}>
+          <button
+            type="button"
+            className={styles.mapListToggleBtn}
+            onClick={() => setExpandList(true)}
+          >
+            <span
+              className={`${styles.listIcon} ${
+                expandList ? styles.activeView : ""
+              }`}
+            />
+          </button>
+          <button
+            type="button"
+            className={styles.mapListToggleBtn}
+            onClick={() => setExpandList(false)}
+          >
+            <span
+              className={`${styles.mapIcon} ${
+                !expandList ? styles.activeView : ""
+              }`}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
