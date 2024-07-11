76308d0991aab47fb9a8de7e805b4dc7e2eeac39
Ross Chapman <rosschapman@users.noreply.github.com>
2024-07-11 16:30:45 -0700

    [OUR415-124] Adds Secondary Navigation bar for listing pages (#78)

--- app/App.tsx
+++ app/App.tsx
@@ -6,6 +6,7 @@ import ReactGA_4 from "react-ga4";
 import Intercom from "react-intercom";
 import { Helmet } from "react-helmet-async";
 import { useHistory } from "react-router-dom";
+import { SiteSearchProvider } from "./hooks/SiteSearch";
 import { GeoCoordinates, getLocation, whiteLabel, AppProvider } from "./utils";
 import { Navigation, UserWay } from "./components/ui";
 
@@ -46,28 +47,30 @@ export const App = () => {
   return (
     <div id={OUTER_CONTAINER_ID} className={styles.outerContainer}>
       <AppProvider userLocation={userLocation}>
-        <Helmet>
-          <title>{title}</title>
-          <meta property="og:url" content={siteUrl} />
-          <meta property="og:title" content={title} />
+        <SiteSearchProvider>
+          <Helmet>
+            <title>{title}</title>
+            <meta property="og:url" content={siteUrl} />
+            <meta property="og:title" content={title} />
 
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@sheltertechorg" />
-          <meta
-            property="og:description"
-            content="Get guided help finding food, housing, health resources and more in San Francisco"
-          />
-          <meta property="og:image" content={MetaImage} />
-          <meta property="og:type" content="website" />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-        </Helmet>
-        {userWay && <UserWay appID={config.SFFAMILIES_USERWAY_APP_ID} />}
-        {intercom && config.INTERCOM_APP_ID && (
-          <Intercom appID={config.INTERCOM_APP_ID} />
-        )}
-        <Navigation />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@sheltertechorg" />
+            <meta
+              property="og:description"
+              content="Get guided help finding food, housing, health resources and more in San Francisco"
+            />
+            <meta property="og:image" content={MetaImage} />
+            <meta property="og:type" content="website" />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="1200" />
+            <meta property="og:image:height" content="630" />
+          </Helmet>
+          {userWay && <UserWay appID={config.SFFAMILIES_USERWAY_APP_ID} />}
+          {intercom && config.INTERCOM_APP_ID && (
+            <Intercom appID={config.INTERCOM_APP_ID} />
+          )}
+          <Navigation />
+        </SiteSearchProvider>
       </AppProvider>
     </div>
   );
--- app/components/ui/BackButton.tsx
+++ app/components/ui/BackButton.tsx
@@ -4,21 +4,21 @@ import { Button } from "components/ui/inline/Button/Button";
 import { useHistory } from "react-router-dom";
 
 // Renders a smart back link that handles pages visited directly or from referring page
-export const BackNavigation = ({
+export const BackButton = ({
   defaultReturnTo,
   children,
 }: {
-  defaultReturnTo?: string;
+  defaultReturnTo: string;
   children: string;
 }) => {
   const history = useHistory();
-
-  // The "POP" action is reserved as a default action for newly created history objects. `react-router-dom` depends on
-  // another history library that defines this behavior. Read more:
+  // The "POP" action is reserved as a default action for newly created history objects. -- in other words, direct entry
+  // `react-router-dom` depends on another history library that defines this behavior. Read more:
   // https://github.com/remix-run/history/blob/main/docs/api-reference.md#reference
-  const backDestination = defaultReturnTo
-    ? () => history.push(defaultReturnTo)
-    : () => history.goBack();
+  const backDestination =
+    history.action === "POP"
+      ? () => history.push(defaultReturnTo)
+      : () => history.goBack();
 
   return (
     <Button onClick={backDestination} variant="linkWhite" arrowVariant="before">
