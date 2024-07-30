2564c1c80efd20ee3853031ce6da1dfad6f9ad8c
Ross Chapman <ross.chapman@exygy.com>
2024-07-30 07:55:32 -0700

    Pulls out error message

--- app/components/ui/Cards/FormattedDate.tsx
+++ app/components/ui/Cards/FormattedDate.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import formatEventDate from "utils/formatEventDate";
 import formatEventTime from "utils/formatEventTime";
 
+const ERROR_MESSAGE = "Invalid Date: missing property {startdate}";
+
 export const FormattedDate = ({
   calendarEvent,
 }: {
   calendarEvent: StrapiModel.CalendarEvent;
 }): JSX.Element => {
   if (calendarEvent?.startdate === undefined) {
-    return <em>{`Invalid Date: missing property {startdate}`}</em>;
+    return <em>{ERROR_MESSAGE}</em>;
   }
   const formattedStartDate = formatEventDate(calendarEvent.startdate);
   const formattedEndDate = formatEventDate(calendarEvent.enddate || "");
