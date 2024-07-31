e12fb806538dac1191a298acd3344beb276f8753
Ross Chapman <ross.chapman@exygy.com>
2024-07-30 21:24:00 -0700

    Fixes timezone

--- app/utils/formatEventDate.ts
+++ app/utils/formatEventDate.ts
@@ -1,8 +1,11 @@
 export default function formatEventDate(date: string) {
-  return new Date(date).toLocaleDateString("en-US", {
+  // The Date constructor is inconsistent depending on the date formatting. For example, it will parse dates formatted
+  // as yyyy-mm-dd as UTC. With slashes it will be parsed as "local time" -- not based on actual locale timezone, but
+  // perceived relative local time.
+  const toFormat = date.replace("-", "/");
+  return new Date(toFormat).toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
-    timeZone: "America/Los_Angeles",
   });
 }
