0f2e1aa97c2c175dff94663ac188469dff467106
Adrien Young <adrien.elizabeth@gmail.com>
2024-08-12 13:33:40 -0700

    Update NavigationFocusReset only on top-level page changes not search query changes

--- app/components/ui/Navigation/NavigationFocusReset.tsx
+++ app/components/ui/Navigation/NavigationFocusReset.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
-// Resets focus state for a11y, by focusing and unfocusing an off-screen element at the top of the page when page location changes
-
-export const NavigationFocusReset = () => {
+/* 
+  Resets focus state for a11y, by focusing and unfocusing an off-screen element at the top of the page when page location changes
+  Since filters change the location, the logic is to only reset focus when a main-level page change occurs (not when user filters a search)
+*/
+export const NavigationFocusReset: React.FC = () => {
   const location = useLocation();
   const hiddenInputRef = useRef<HTMLInputElement | null>(null);
+  const previousPathSegmentRef = useRef<string>("");
 
   useEffect(() => {
-    if (hiddenInputRef.current) {
-      hiddenInputRef.current.focus();
-      hiddenInputRef.current.blur();
+    const currentPathSegment = location.pathname.split("/")[1];
+
+    if (currentPathSegment !== previousPathSegmentRef.current) {
+      if (hiddenInputRef.current) {
+        hiddenInputRef.current.focus();
+        hiddenInputRef.current.blur();
+      }
+      previousPathSegmentRef.current = currentPathSegment;
     }
   }, [location]);
 
