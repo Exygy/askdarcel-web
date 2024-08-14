fcce9e458c97fe3eecfdf928ad03ff6001a07ae1
Adrien Young <adrien.elizabeth@gmail.com>
2024-08-14 13:01:31 -0700

    use location.search instead to only check if query string changes

--- app/components/ui/Navigation/NavigationFocusReset.tsx
+++ app/components/ui/Navigation/NavigationFocusReset.tsx
@@ -2,8 +2,9 @@ import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 /* 
-  Resets focus state for a11y by focusing and unfocusing an off-screen element at the top of the page when the page location changes.
-  The focus reset occurs on all page location changes except for top-level category and search page filter changes.
+  Resets focus state for a11y by focusing and unfocusing an off-screen element 
+  at the top of the page when the page location changes.
+  The focus reset occurs on all page location changes except when only the query string changes.
 */
 export const NavigationFocusReset: React.FC = () => {
   const location = useLocation();
@@ -11,21 +12,14 @@ export const NavigationFocusReset: React.FC = () => {
   const previousPathRef = useRef<string>("");
 
   useEffect(() => {
-    const currentPath = location.pathname;
+    const currentPath = location.pathname + location.search;
 
-    const isCategoryPage = currentPath.includes("results");
-    const isSearchPage = currentPath.includes("query");
-    const pathHasFilters = isCategoryPage || isSearchPage;
-
-    // Check if the current path is different from the previous path
     const pathChanged = currentPath !== previousPathRef.current;
 
-    // Focus reset should occur if the path changed and it's not just a filter change
-    if (pathChanged && (!pathHasFilters || previousPathRef.current === "")) {
-      if (hiddenInputRef.current) {
-        hiddenInputRef.current.focus();
-        hiddenInputRef.current.blur();
-      }
+    // Focus reset should occur if the path changed and it's not just a query string change
+    if (pathChanged && !location.search && hiddenInputRef.current) {
+      hiddenInputRef.current.focus();
+      hiddenInputRef.current.blur();
     }
 
     previousPathRef.current = currentPath;
