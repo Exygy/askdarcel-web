1324459109ab39260f7139bdde93283343060fb1
Ross Chapman <ross.chapman@exygy.com>
2024-09-04 13:12:05 -0700

    Checkpoint

--- app/components/search/Refinements/RefinementListFilter.tsx
+++ app/components/search/Refinements/RefinementListFilter.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { CurrentRefinements } from "react-instantsearch";
+import { CurrentRefinements, useCurrentRefinements } from "react-instantsearch";
 import styles from "./RefinementFilters.module.scss";
 
-const RefinementListFilter = () => {
+const RefinementListFilter = (props: any) => {
+  const { items, canRefine, refine } = useCurrentRefinements(props);
+  const foo = props.transformItems;
   return (
-    TODODODODOD<CurrentRefinements>
-      <ul>
-        {items.map((item: any) => (
-          <li key={item.label}>
-            <label className={styles.checkBox}>
-              {item.label}
-              <input
-                className={styles.refinementInput}
-                type="checkbox"
-                checked={item.isRefined}
-                onChange={() => {
-                  refine(item.value);
-                }}
-              />
-            </label>
-          </li>
-        ))}
-      </ul>
-    </CurrentRefinements>
+    <ul>
+      {foo(items).map((item: any) => (
+        <li key={item.label}>
+          <label className={styles.checkBox}>
+            {item.label}
+            <input
+              className={styles.refinementInput}
+              type="checkbox"
+              checked={item.isRefined}
+              onChange={() => {
+                refine(item.value);
+              }}
+            />
+          </label>
+        </li>
+      ))}
+    </ul>
   );
 };
 
