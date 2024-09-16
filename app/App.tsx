ce6533533e4238cbd0f49963bfcd3cbdb386348e
Ross Chapman <ross.chapman@exygy.com>
2024-09-16 16:07:31 -0400

    Checkpoint: removes translation code

--- app/App.tsx
+++ app/App.tsx
@@ -15,87 +15,14 @@ import { Footer, Navigation, Loader } from "./components/ui";
 
 import MetaImage from "./assets/img/Our415_OG.png";
 import styles from "./App.module.scss";
-import { useLocation } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import qs, { ParsedQs } from "qs";
-import { translate } from "utils/DataService";
-import { AroundRadius } from "algoliasearch";
 import config from "./config";
 
 const { siteUrl, title } = websiteConfig;
 export const OUTER_CONTAINER_ID = "outer-container";
 
-interface ConfigureState {
-  aroundRadius?: AroundRadius;
-  [key: string]: any;
-}
-
-const INDEX_NAME = `${config.ALGOLIA_INDEX_PREFIX}_services_search`;
-
-interface SearchState extends ParsedQs {
-  configure?: ConfigureState;
-  query?: string | null;
-  [key: string]: any;
-}
-
 export const App = () => {
-  const [cookies] = useCookies(["googtrans"]);
-  const { search } = useLocation();
   const history = useHistory();
   const [userLocation, setUserLocation] = useState<GeoCoordinates | null>(null);
-  const [searchState, setSearchState] = useState<SearchState | null>(null);
-
-  // In cases where we translate a query into English, we use this value
-  // to represent the user's original, untranslated input. The untranslatedQuery
-  // is displayed in the UI and stored in the URL params.
-  // This untranslatedQuery value is also checked against when a new search is triggered
-  // to determine if the user has input a different query (vs. merely selecting refinements),
-  // in which case we need to call the translation API again
-  const [untranslatedQuery, setUntranslatedQuery] = useState<string | null>(
-    null
-  );
-  const [translatedQuery, setTranslatedQuery] = useState<string | null>(null);
-  const [nonQuerySearchParams, setNonQuerySearchParams] = useState<SearchState>(
-    {}
-  );
-
-  useEffect(() => {
-    const qsParams = qs.parse(search.slice(1));
-    setUntranslatedQuery(
-      qsParams[`${INDEX_NAME}.query}`]
-        ? (qsParams[`${INDEX_NAME}.query}`] as string)
-        : ""
-    );
-    delete qsParams["production_services_search[query]"];
-    setNonQuerySearchParams(qsParams);
-  }, [search]);
-
-  useEffect(() => {
-    if (untranslatedQuery === null) {
-      return;
-    }
-
-    let queryLanguage = "en";
-    // Google Translate determines translation source and target with a
-    // "googtrans" cookie. If the cookie exists, we assume that the
-    // the query should be translated into English prior to querying Algolia
-    const translationCookie = cookies.googtrans;
-    if (translationCookie) {
-      [, queryLanguage] = translationCookie.split("/en/");
-    }
-
-    const emptyQuery = untranslatedQuery.length === 0;
-    if (queryLanguage === "en" || emptyQuery) {
-      setTranslatedQuery(untranslatedQuery);
-    } else if (untranslatedQuery) {
-      translate(untranslatedQuery, queryLanguage).then((result) =>
-        setTranslatedQuery(result)
-      );
-    }
-  }, [untranslatedQuery, cookies.googtrans]);
-  useEffect(() => {
-    setSearchState({ ...nonQuerySearchParams, query: translatedQuery });
-  }, [translatedQuery, nonQuerySearchParams]);
 
   useEffect(() => {
     getLocation().then((loc) => {
