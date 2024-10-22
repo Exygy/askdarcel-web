392538ecd07009d3e360efed8b5926c3b7f334fc
Ross Chapman <ross.chapman@exygy.com>
2024-10-22 14:13:29 -0700

    Adds back default

--- app/components/ui/LabelTag.tsx
+++ app/components/ui/LabelTag.tsx
@@ -4,11 +4,12 @@ import styles from "./LabelTag.module.scss";
 
 // This component relies on the shape and naming conventions of data Shelter
 // Tech syncs to Algolia which distinguishes service and organization results
-// with a "type" field.
-//
-// CAVEAT: It's unlikely this will change without our knowledge but developers
+// with a "type" field. It's unlikely this will change without our knowledge but developers
 // should be aware of the possibility.
+//
+// THOUGHT: replacing with enum if another type is added
 const SHELTER_TECH_TYPE_FOR_ORGANIZATION = "resource";
+const SHELTER_TECH_TYPE_FOR_SERVICE = "service";
 
 export const LabelTag = ({ label }: { label: string }) => {
   if (label === SHELTER_TECH_TYPE_FOR_ORGANIZATION) {
@@ -19,9 +20,13 @@ export const LabelTag = ({ label }: { label: string }) => {
     );
   }
 
-  return (
-    <span className={classNames(styles.labelTag, styles.serviceType)}>
-      Service
-    </span>
-  );
+  if (label === SHELTER_TECH_TYPE_FOR_SERVICE) {
+    return (
+      <span className={classNames(styles.labelTag, styles.serviceType)}>
+        Service
+      </span>
+    );
+  }
+
+  return <span className={classNames(styles.labelTag)}>{label}</span>;
 };
