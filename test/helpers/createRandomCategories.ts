e01c50c91c59ccd76fc59a38744384dceb7f88a7
Ross Chapman <ross.chapman@exygy.com>
2024-11-06 13:56:06 -0800

    Updates test comment and incrementing

--- test/helpers/createRandomCategories.ts
+++ test/helpers/createRandomCategories.ts
@@ -1,10 +1,18 @@
 import { faker } from "@faker-js/faker";
 
+/**
+ * Generates an object of categories for use in search refinement components
+ *
+ * @param size Customize the number of categories
+ * @returns
+ */
 export function createRandomCategories(size: number) {
   const result: { [key: string]: number } = {};
 
   for (let i = 0; i < size; i++) {
-    result[`${faker.company.name()}_${size}`] = faker.number.int(100);
+    // Append incrementor because the current version of faker does not support
+    // uniqueness
+    result[`${faker.company.name()}_${i}`] = faker.number.int(100);
   }
 
   return result;
