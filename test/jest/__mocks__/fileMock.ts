4826a8e1de96bb44df4df77e42d86442fb703bf7
Ross Chapman <ross.chapman@exygy.com>
2024-11-07 08:55:51 -0800

    Mocks config

--- test/jest/__mocks__/fileMock.ts
+++ test/jest/__mocks__/fileMock.ts
@@ -1,3 +1,4 @@
 // Webpack is not available in tests to handle file imports. Our test config
 // tells jest to replace any file imports it sees with this empty string.
-export default "";
+const str = "";
+export default str;
