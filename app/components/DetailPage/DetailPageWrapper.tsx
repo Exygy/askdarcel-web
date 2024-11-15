9b193e59103d14e2799524df469161763e83a248
Adrien Young <adrien.elizabeth@gmail.com>
2024-11-15 10:06:26 -0800

    listing -> DetailPage

--- app/components/DetailPage/DetailPageWrapper.tsx
+++ app/components/DetailPage/DetailPageWrapper.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import { ActionSidebar } from "components/listing";
+import { ActionSidebar } from "components/DetailPage";
 import styles from "./DetailPageWrapper.module.scss";
 import { OrganizationAction } from "models";
 
@@ -19,15 +19,15 @@ const DetailPageWrapper = ({
   sidebarActions,
   onClickAction,
 }: DetailPageWrapperProps) => (
-  <div className={styles[`listing-wrapper`]}>
+  <div className={styles[`detail-wrapper`]}>
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
     </Helmet>
-    <article className={styles.listing} id="resource">
-      <div className={styles["listing--main"]}>
-        <div className={styles["listing--main--left"]}>{children}</div>
-        <aside className={`${styles["listing--aside"]} no-print`}>
+    <article className={styles.detail} id="resource">
+      <div className={styles["detail--main"]}>
+        <div className={styles["detail--main--left"]}>{children}</div>
+        <aside className={`${styles["detail--aside"]} no-print`}>
           <ActionSidebar
             actions={sidebarActions}
             onClickAction={onClickAction}
