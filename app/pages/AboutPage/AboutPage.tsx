dbadb661fb7679d3ee40e032bb2c93c6dc0b9218
Ross Chapman <ross.chapman@exygy.com>
2024-11-26 12:27:28 -0800

    Removes imports

--- app/pages/AboutPage/AboutPage.tsx
+++ app/pages/AboutPage/AboutPage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Loader } from "components/ui/Loader";
 import { usePageContent } from "hooks/StrapiAPI";
 import { Masthead } from "../../components/ui/Masthead/Masthead";
-import { EmailSignup } from "../../components/EmailSignup/Emailsignup";
 import { TwoColumnContentSection } from "../../components/ui/TwoColumnContentSection/TwoColumnContentSection";
 import { PageContent, StrapiDatum } from "models/Strapi";
 
