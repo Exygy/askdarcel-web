673275513ac61601068a92a528e45d5425c416de
Adrien Young <adrien.elizabeth@gmail.com>
2025-02-06 09:27:40 -0800

    add dcyf finalized subcats and eligibilities to refinementMappings.ts

--- app/utils/refinementMappings.ts
+++ app/utils/refinementMappings.ts
@@ -1,34 +1,6 @@
 // On search results page, not category listings page
 
-export const eligibilitiesMapping = {
-  Disability: [
-    "All Disabilities",
-    "Developmental Disability",
-    "Physical Disability",
-    "Learning Disability",
-    "Intellectual Disability",
-    "Visual Impairment",
-    "Limited Mobility",
-    "Deaf or Hard of Hearing",
-  ],
-  Families: ["Families", "Families with Babies"],
-  Homeless: ["Homeless"],
-  Immigrants: ["Immigrants", "Undocumented"],
-  "LGBTQ+": ["LGBTQ+", "LGBTQ", "Transgender and Gender Non-Conforming"], // Todo: "LGBTQ" can be removed from array once LGBTQ eligibility has been switched to LGBTQ+ in DB across all environments
-  "Mental Health/Substance Use": ["Mental Illness", "Substance Dependency"],
-  "Re-Entry": ["Re-Entry", "In Jail"],
-  "Seniors (55+ years old)": ["Seniors (55+ years old)"],
-  "Transition Aged Youth": ["Transition Aged Youth", "Teens (13-19 years old)"],
-  "Trauma Survivors": [
-    "Trauma Survivors",
-    "Abuse or Neglect Survivors",
-    "Domestic Violence Survivors",
-    "PTSD",
-  ],
-  Veterans: ["Veterans"],
-};
-
-// Hardcoded catgories in search page sidebar (Can add multiple categories as array)
+// Hardcoded categories in search page sidebar (Can add multiple categories as array)
 /* i.e. "Arts, Culture & Identity": [
   "Arts and Creative Expression",
   "Culinary Arts",
@@ -41,6 +13,8 @@ export const eligibilitiesMapping = {
   "Summer Programs"
 ]
 */
+
+// TODO: Check why these are still being used
 export const categoriesMapping = {
   "Basic Needs & Shelter": ["Basic Needs & Shelter"],
   Employment: ["Employment"],
@@ -130,4 +104,185 @@ export const our415SubcategoriesWithDuplicates = [
   "Youth Leadership",
 ];
 
-export const our415Subcategories = new Set(our415SubcategoriesWithDuplicates);
+export const our415SubcategoryNames = new Set(
+  our415SubcategoriesWithDuplicates
+);
+
+// const eligibilitiesToDisplay = [
+//   "African American",
+//   "Asian",
+//   "Latinx",
+//   "Pacific Islander",
+//   "Middle Eastern and North African",
+//   "Native American",
+//   "Filipino/a",
+//   "Preschool",
+//   "Elementary School",
+//   "Middle School Students",
+//   "High School Students",
+//   "College Students",
+//   "City College of SF",
+//   "Low-Income",
+//   "Underinsured",
+//   "Uninsured",
+//   "Benefit Recipients",
+//   "Foster Youth",
+//   "I am Experiencing Homelessness",
+//   "Undocumented",
+//   "CIP (Children of Incarcerated Parents)",
+//   "Trauma Survivors",
+//   "Teen Parents",
+//   "Non-Binary",
+//   "Transgender and Gender Non-Conforming",
+//   "Boys",
+//   "Men",
+//   "Girls",
+//   "Women",
+//   "LGBTQ+",
+//   "Youth",
+//   "Teens",
+//   "Toddler",
+//   "Families with children below 18 years old",
+//   "Pre-Teen",
+//   "Infants",
+//   "All Ages",
+//   "TAY",
+//   "Children",
+//   "Special Needs/Disabilities",
+//   "Mental and Behavioral Health Challenges",
+//   "Deaf or Hard of Hearing",
+//   "Visual Impairment",
+//   "Limited Mobility",
+//   "Spanish",
+//   "Chinese",
+//   "Filipino",
+//   "ESL/ELL (English Language Learner)",
+// ];
+
+// const allEligibilities = [
+//   "API (Asian/Pacific Islander", // Has results if Pacific Islander does not
+//   "African American",
+//   "All Ages",
+//   "Asian",
+//   "Benefit Recipients", // No results
+//   "Boys",
+//   "CIP (Children of Incarcerated Parents)",
+//   "Children", // From DCYF, no results
+//   "Children (0-13 years old)", // Has results if Children does not
+//   "Chinese",
+//   "City College of SF", // From DCYF, no results
+//   "College Students", // Has results, while College does not
+//   "College", // From DCYF, no results
+//   "Deaf or Hard of Hearing", // From DCYF, no results
+//   "Elementary School", // From DCYF, no results
+//   "Elementary School Student", // Has results if Elementary School does not
+//   "ESL/ELL (English Language Learner)",
+//   "Families with children below 18 years old",
+//   "Filipino", // From DCYF, no results
+//   "Filipino/a",
+//   "Foster Youth",
+//   "Girls",
+//   "High School Students", // From DCYF, no results
+//   "High School", // Has results if High School Students does not
+//   "I am Experiencing Homelessness", // From DCYF, no results
+//   "Experiencing Homelessness", // Has results if I am Experiencing Homelessness does not
+//   "Infants", // From DCYF, no results
+//   "Infants (0-2 years old)", // Has results if Infants does not
+//   "Latinx", // From DCYF, no results
+//   "Hispanic/Latinx", // Has results if Latinx does not
+//   "LGBTQ+",
+//   "Limited Mobility", // From DCYF, no results
+//   "Low-Income",
+//   "Mental and Behavioral Health Challenges", // From DCYF, no results
+//   "Men",
+//   "Middle Eastern and North African",
+//   "Middle School Students", // From DCYF, no results
+//   "Middle School", // Has results if Middle School Students does not
+//   "Native American",
+//   "Non-Binary",
+//   "Pacific Islander",
+//   "Preschool", // From DCYF, no results
+//   "Preschool Student", // Has results if Preschool does not
+//   "Pre-Teen", // From DCYF, no results
+//   "Preteen", // Has results if Pre-Teen does not
+//   "Spanish", // From DCYF, no results
+//   "Special Needs/Disabilities",
+//   "TAY", // From DCYF, no results
+//   "Transitional Aged Youth (TAY)", // Has results if TAY does not
+//   "Teen Parent", // Has results if Teen Parents does not
+//   "Teen Parents", // From DCYF, no results
+//   "Teens", // From DCYF, no results
+//   "Teens (13-18 years old)", // Has results if Teens does not
+//   "Toddler", // From DCYF, no results
+//   "Toddler Age", // Has results if Toddler does not
+//   "Transgender and Gender Non-Conforming",
+//   "Trauma Survivors",
+//   "Underinsured",
+//   "Undocumented",
+//   "Uninsured",
+//   "Visual Impairment",
+//   "Women",
+//   "Youth", // From DCYF, no results
+//   "Youth (below 21 years old)", // Has results if Youth does not
+// ];
+
+export const our415EligibilitiesMapping = {
+  "African American": ["African American"],
+  Asian: ["Asian"],
+  Latinx: ["Hispanic/Latinx", "Latinx"],
+  "Pacific Islander": ["API (Asian/Pacific Islander", "Pacific Islander"],
+  "Middle Eastern and North African": ["Middle Eastern and North African"],
+  "Native American": ["Native American"],
+  "Filipino/a": ["Filipino", "Filipino/a"],
+  Preschool: ["Preschool Student", "Preschool"],
+  "Elementary School": ["Elementary School Student", "Elementary School"],
+  "Middle School Students": ["Middle School", "Middle School Students"],
+  "High School Students": ["High School", "High School Students"],
+  "College Students": ["College", "College Students"],
+  "City College of SF": ["City College of SF"],
+  "Low-Income": ["Low-Income"],
+  Underinsured: ["Underinsured"],
+  Uninsured: ["Uninsured"],
+  "Benefit Recipients": ["Benefit Recipients"],
+  "Foster Youth": ["Foster Youth"],
+  "I am Experiencing Homelessness": [
+    "Experiencing Homelessness",
+    "I am Experiencing Homelessness",
+  ],
+  Undocumented: ["Undocumented"],
+  "CIP (Children of Incarcerated Parents)": [
+    "CIP (Children of Incarcerated Parents)",
+  ],
+  "Trauma Survivors": ["Trauma Survivors"],
+  "Teen Parents": ["Teen Parent", "Teen Parents"],
+  "Non-Binary": ["Non-Binary"],
+  "Transgender and Gender Non-Conforming": [
+    "Transgender and Gender Non-Conforming",
+  ],
+  Boys: ["Boys"],
+  Men: ["Men"],
+  Girls: ["Girls"],
+  Women: ["Women"],
+  "LGBTQ+": ["LGBTQ+"],
+  Youth: ["Youth (below 21 years old)", "Youth"],
+  Teens: ["Teens (13-18 years old)", "Teens"],
+  Toddler: ["Toddler Age", "Toddler"],
+  "Families with children below 18 years old": [
+    "Families with children below 18 years old",
+  ],
+  "Pre-Teen": ["Preteen", "Pre-Teen"],
+  Infants: ["Infants (0-2 years old)", "Infants"],
+  "All Ages": ["All Ages"],
+  TAY: ["Transitional Aged Youth (TAY)", "TAY"],
+  Children: ["Children (0-13 years old)", "Children"],
+  "Special Needs/Disabilities": ["Special Needs/Disabilities"],
+  "Mental and Behavioral Health Challenges": [
+    "Mental and Behavioral Health Challenges",
+  ],
+  "Deaf or Hard of Hearing": ["Deaf or Hard of Hearing"],
+  "Visual Impairment": ["Visual Impairment"],
+  "Limited Mobility": ["Limited Mobility"],
+  Spanish: ["Spanish"],
+  Chinese: ["Chinese"],
+  "ESL/ELL (English Language Learner)": ["ESL/ELL (English Language Learner)"],
+};
