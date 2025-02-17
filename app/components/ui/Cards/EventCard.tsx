0e43c902a56e711ad48932f54c4e82cb49759180
Adrien Young <adrien.elizabeth@gmail.com>
2025-02-17 09:18:25 -0800

    change event links to external links since we don't have event pages

--- app/components/ui/Cards/EventCard.tsx
+++ app/components/ui/Cards/EventCard.tsx
@@ -22,7 +22,13 @@ export const EventCard = ({ event }: { event: EventResponse }) => {
 
       <div className={styles.content}>
         <h3 className={styles.contentTitle}>
-          <a href={linkUrl} data-testid={"eventcard-link"}>
+          <a
+            href={linkUrl}
+            data-testid={"eventcard-link"}
+            // While events are external links:
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             {title}
           </a>
         </h3>
